11a915039f6bf045e7d90350b08495d302fa4c69
niazieo <omar.niazie@gmail.com>
2023-01-01 21:53:20 -0700

    Revert "Movie List functionality implemented"
    This reverts commit 439e1dce3a01db057d962d82bd89fb8fc6857952.

--- src/bot.js
+++ src/bot.js
@@ -15,15 +15,6 @@ const client = new Client({
     ]
 });
 
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
-
 client.commands = new Collection();
 client.commandArray = [];
 client.snipes = new Collection();
