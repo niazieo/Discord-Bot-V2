80b943e7fbebd2c4fec6042ca19143a98c0c9aaf
niazieo <omar.niazie@gmail.com>
2023-03-13 17:49:27 -0600

    fx2

--- src/bot.js
+++ src/bot.js
@@ -21,7 +21,7 @@ const admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert({
     "projectId": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "private_key": JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: "https://discord-bot-74d33.firebaseio.com"
