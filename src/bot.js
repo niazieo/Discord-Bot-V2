0c177e4542de045e113b6f1c400669003945cdc8
niazieo <omar.niazie@gmail.com>
2023-05-23 17:28:42 -0600

    a2

--- src/bot.js
+++ src/bot.js
@@ -21,7 +21,7 @@ const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: process.env.private_key,
+    privateKey: process.env.private_key.replace(/\\n/g, '\n'),
     projectId: process.env.project_id,
     clientEmail: process.env.client_email
   }),
