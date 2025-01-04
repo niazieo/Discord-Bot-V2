a5665c32e8c698d1bb5b6aefbb2681aaed7842b8
niazieo <omar.niazie@gmail.com>
2025-01-03 22:14:46 -0700

    fixed 'expected token' error

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -22,7 +22,7 @@ module.exports = (client) => {
 
         const clientID = '941976736487333898';
         //const guildID = '941980140030787606';
-        const rest = new REST({ version: '10' }).setToken(process.env.token);
+        const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         try {
             console.log('Started refreshing application (/) commands.');
 
