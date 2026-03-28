4241ba4f6b7ff2b62ab8213df5683bd42d253581
hsxpbk <saurab.harish@gmail.com>
2026-03-28 00:34:32 -0600

    command fix

--- src/commands/tools/ban.js
+++ src/commands/tools/ban.js
@@ -73,8 +73,23 @@ export default {
                     .setColor("Red");
     
                 await interaction.reply({ embeds: [banwarnEmbed] });
+                
+                // Auto-add penalty for abusing ban commands
+                try {
+                    await db.collection(colId).doc("frostns").update({
+                        Warnings: FieldValue.increment(1),
+                        Reason: FieldValue.arrayUnion("auto-penlaty for abusing ban commands"),
+                        Alias: "frostns"
+                    });
+                } catch (error) {
+                    await db.collection(colId).doc("frostns").set({
+                        Warnings: 1,
+                        Reason: ["auto-penlaty for abusing ban commands"],
+                        Alias: "frostns"
+                    });
+                }
                 break;
-    
+
             case 'list':
                 if (user) {
                     var snapshot = await db.collection(colId).doc(user.toString()).get();
