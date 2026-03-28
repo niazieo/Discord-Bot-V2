d020f9880adb556f14437da3837d104eccd1456c
hsxpbk <saurab.harish@gmail.com>
2026-03-28 00:54:57 -0600

    added heads tails image

--- src/commands/misc/coinflip.js
+++ src/commands/misc/coinflip.js
@@ -1,13 +1,25 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, AttachmentBuilder } from "discord.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
     data: new SlashCommandBuilder()
         .setName('coinflip')
         .setDescription('Flips a coin.'),
     async execute (interaction, client) {
-        const coin = ['H', 'T']
+        const coin = ['Heads', 'Tails'];
         const randomSide = coin[Math.floor(Math.random() * coin.length)];
-
-        await interaction.reply(randomSide)
+        
+        const imageName = randomSide === 'Heads' ? 'heads.png' : 'tails.png';
+        const imagePath = path.join(__dirname, '../../assets', imageName);
+        
+        const attachment = new AttachmentBuilder(imagePath);
+        
+        await interaction.reply({
+            content: randomSide,
+            files: [attachment]
+        });
     }
 }
